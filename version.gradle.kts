91060ef285559dcbacc0a0de5f1dea72d5233c3d
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-10-10 16:58:40 +0300

    Bump versions

--- version.gradle.kts
+++ version.gradle.kts
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-val spineBaseVersion: String by extra("2.0.0-SNAPSHOT.88")
-val mcJavaVersion: String by extra("2.0.0-SNAPSHOT.91")
-val spineCoreVersion: String by extra("2.0.0-SNAPSHOT.94")
-val versionToPublish: String by extra(spineCoreVersion)
+val baseVersion: String by extra("2.0.0-SNAPSHOT.112")
+val mcJavaVersion: String by extra("2.0.0-SNAPSHOT.101")
+val coreVersion: String by extra("2.0.0-SNAPSHOT.113")
+val versionToPublish: String by extra("2.0.0-SNAPSHOT.9999")
